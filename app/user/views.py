2b5cfcb4b2d6261ec0e90bee9e845742262f9994
Pastor Darling <andafadarling@gmail.com>
2024-01-30 20:45:23 +0100

    setup custom user and swagger documentation

---
+++ app/user/views.py
@@ -0,0 +1,10 @@
+"""
+Views  fpr tje iser Api.
+"""
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in a system"""
+    serializer_class = UserSerializer
