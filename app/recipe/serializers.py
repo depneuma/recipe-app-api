2e9393e3f0d3313342f1db1e947f61ba978c07bf
Pastor Darling <andafadarling@gmail.com>
2024-02-15 20:49:28 +0100

    Configured api to handle static and media files

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -83,4 +83,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for Recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to Recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
